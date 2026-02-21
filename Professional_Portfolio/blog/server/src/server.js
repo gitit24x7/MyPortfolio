fafe506dce9383cdc4cf0e97b327ece1ad67dfd8
gitit24x7 <thisisadityaojha@gmail.com>
2026-02-22 01:01:47 +0530

    correcting the syntax error

--- Professional_Portfolio/blog/server/src/server.js
+++ Professional_Portfolio/blog/server/src/server.js
@@ -71,7 +71,7 @@ const allowedOrigins = [
     'https://www.adityaojha.dev',
     'https://adityaojha.dev',
     'http://localhost:5173',  // Vite dev server
-    'http://localhost:3000'
+    'http://localhost:3000',
     'https://adityaojha.vercel.app'
 ];
 
